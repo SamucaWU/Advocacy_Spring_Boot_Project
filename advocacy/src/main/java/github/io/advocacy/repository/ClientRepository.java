948a05d3ee8a4f6eb32f9d6e08f0cc3c661da4be
Samuel M. Dias <samucaWhatsUp@gmail.com>
2026-03-14 11:15:22 -0300

    Ajuste nos Repository´s

--- advocacy/src/main/java/github/io/advocacy/repository/ClientRepository.java
+++ advocacy/src/main/java/github/io/advocacy/repository/ClientRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface ClientRepository extends JpaRepository<ClientEntity, Integer> {
+public interface ClientRepository extends JpaRepository<ClientEntity, Long> {
 
 }
--- advocacy/src/main/java/github/io/advocacy/repository/LawyerRepository.java
+++ advocacy/src/main/java/github/io/advocacy/repository/LawyerRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LawyerRepository extends JpaRepository<LawyerRepository, Integer> {
+public interface LawyerRepository extends JpaRepository<LawyerRepository, Long> {
 
 }
